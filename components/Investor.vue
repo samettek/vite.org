745fb5d5a958f778cec5cea4cbeab6796135dc84
soliury <lingyong1992@foxmail.com>
2018-05-07 14:34:13 +0800

    update investor

--- components/Investor.vue
+++ components/Investor.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="columns is-multiline is-centered">
-    <div :key="item.key" v-for="item in list" class="column is-one-quarter">
+  <div class="columns is-multiline is-centered is-mobile">
+    <div :key="item.key" v-for="item in list" class="column is-one-fifth is-4-mobile">
       <img class="investor-logo" :src="item.logo" alt="">
     </div>
   </div>
@@ -18,7 +18,19 @@
             },
             {
               key: 'hotCapital',
-              logo: require('~/assets/images/investor/hot_capital.jpg')
+              logo: require('~/assets/images/investor/hot_capital.png')
+            },
+            {
+              key: 'dazhi',
+              logo: require('~/assets/images/investor/dazhi.png')
+            },
+            {
+              key: 'handsome',
+              logo: require('~/assets/images/investor/handsome.png')
+            },
+            {
+              key: 'hesheng',
+              logo: require('~/assets/images/investor/hesheng.png')
             }
           ]
         }
@@ -29,5 +41,9 @@
 <style rel="stylesheet/scss" lang="scss" scoped>
   .column {
     text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 </style>
