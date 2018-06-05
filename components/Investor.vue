d8c35380de82c87d5aeac425b86e7d023c73bc32
soliury <lingyong1992@foxmail.com>
2018-06-05 14:46:40 +0800

    update investor

--- components/Investor.vue
+++ components/Investor.vue
@@ -4,11 +4,15 @@
       <h2 class="title section-title inview1 animated fadeInDown delay-0-800">
         {{$t('investor.title')}}
       </h2>
-      <div class="columns is-multiline is-centered is-mobile">
-        <div :key="item.key" v-for="item in list" class="column is-2-desktop is-4-touch">
-          <div class="img-wrapper">
-            <img class="investor-logo" :src="item.logo" alt="">
-          </div>
+      <div class="columns is-multiline is-centered is-mobile is-gapless">
+        <div :key="item.key" v-for="(item, index) in list" class="column is-2-desktop is-3-touch">
+          <a :href="item.url" target="_blank">
+            <div class="img-wrapper" @mouseover="onHover(index)"
+                 @mouseout="onOut(index)">
+              <img v-show="index === hover" class="investor-logo" :src="item.logoActive" alt="">
+              <img v-show="index !== hover" class="investor-logo" :src="item.logo" alt="">
+            </div>
+          </a>
         </div>
       </div>
     </div>
@@ -20,64 +24,92 @@
       name: 'investment',
       data () {
         return {
+          hover: null,
           list: [
             {
               key: 'bitmain',
-              logo: require('~/assets/images/investor/bitmain-black.png')
+              logo: require('~/assets/images/investor/bitmain.png'),
+              logoActive: require('~/assets/images/investor/bitmain-blue.png'),
+              url: 'https://www.bitmain.com/'
             },
-            // {
-            //   key: 'starwinCapital',
-            //   logo: require('~/assets/images/investor/starwin-capital.jpg')
-            // },
+            {
+              key: 'starwinCapital',
+              logo: require('~/assets/images/investor/starwin.png'),
+              logoActive: require('~/assets/images/investor/starwin-blue.png')
+            },
             {
               key: 'nodeCapital',
-              logo: require('~/assets/images/investor/node-capital-black.png')
+              logo: require('~/assets/images/investor/node.png'),
+              logoActive: require('~/assets/images/investor/node-blue.png'),
+              url: 'http://www.nodecap.com/'
             },
             {
               key: 'linkvc',
-              logo: require('~/assets/images/investor/linkvc.png')
+              logo: require('~/assets/images/investor/linkvc.png'),
+              logoActive: require('~/assets/images/investor/linkvc-blue.png'),
+              url: 'http://www.linkvc.com/'
             },
             {
               key: 'genesisCapital',
-              logo: require('~/assets/images/investor/genesis-capital.png')
+              logo: require('~/assets/images/investor/genesis.png'),
+              logoActive: require('~/assets/images/investor/genesis-blue.png')
             },
             {
               key: 'funcityCapital',
-              logo: require('~/assets/images/investor/funcity-capital.jpg')
+              logo: require('~/assets/images/investor/funcity.png'),
+              logoActive: require('~/assets/images/investor/funcity-blue.png'),
+              url: 'http://www.funcityholding.com/'
             },
             {
               key: 'helloCapital',
-              logo: require('~/assets/images/investor/hello-capital-black.png')
+              logo: require('~/assets/images/investor/hello.png'),
+              logoActive: require('~/assets/images/investor/hello-blue.png'),
+              url: 'http://hellocapital.net/'
             },
             {
               key: 'zhizi',
-              logo: require('~/assets/images/investor/zhizi.png')
+              logo: require('~/assets/images/investor/zhizi.png'),
+              logoActive: require('~/assets/images/investor/zhizi-blue.png')
             },
             {
               key: 'hotCapital',
-              logo: require('~/assets/images/investor/hot-capital.png')
+              logo: require('~/assets/images/investor/hot.png'),
+              logoActive: require('~/assets/images/investor/hot-blue.png')
             },
             {
               key: 'dazhi',
-              logo: require('~/assets/images/investor/dazhi.png')
+              logo: require('~/assets/images/investor/dazhi.png'),
+              logoActive: require('~/assets/images/investor/dazhi-blue.png')
             },
             {
               key: 'handsome',
-              logo: require('~/assets/images/investor/handsome.png')
+              logo: require('~/assets/images/investor/handsome.png'),
+              logoActive: require('~/assets/images/investor/handsome-blue.png')
             },
             {
               key: 'hesheng',
-              logo: require('~/assets/images/investor/hesheng.png')
+              logo: require('~/assets/images/investor/hesheng.png'),
+              logoActive: require('~/assets/images/investor/hesheng-blue.png')
             },
             {
               key: 'b100',
-              logo: require('~/assets/images/investor/b100.png')
+              logo: require('~/assets/images/investor/b100.png'),
+              logoActive: require('~/assets/images/investor/b100-blue.png')
             },
             {
               key: 'bn',
-              logo: require('~/assets/images/investor/bn_capital.png')
+              logo: require('~/assets/images/investor/bn.png'),
+              logoActive: require('~/assets/images/investor/bn-blue.png')
             }
           ]
+        }
+      },
+      methods: {
+        onHover (index) {
+          this.hover = index
+        },
+        onOut () {
+          this.hover = null
         }
       }
     }
@@ -100,7 +132,7 @@
 
   .title {
     font-size:36px;
-    font-family:PingFangSC-Medium;
+    font-family: $font-family-main;
     color:rgba(51,51,51,1);
     line-height:50px;
     text-align: center;
@@ -116,9 +148,12 @@
       display: flex;
       align-items: center;
       justify-content: center;
+      &:hover {
+        cursor: pointer;
+      }
       img {
-        max-height: 200px;
-        max-height: 200px;
+        /*max-height: 180px;*/
+        /*max-width: 180px;*/
       }
     }
   }
