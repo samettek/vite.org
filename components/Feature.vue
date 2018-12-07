d17ba5ad90c845fdff4eead7fc1fd4d08471a8fc
hurrytospring <ddyz1993@163.com>
2018-12-07 19:48:02 +0800

    add

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,135 +1,276 @@
 <template>
-  <div>
-    <section>
-      <h1 style="text-align: center">Vite 特性</h1>
-        <div class="container is-flex">
-          <div>
-            <div class="safe">高吞吐</div>
-            <div class="safe">低延迟</div>
-            <div class="safe">可扩展</div>
-          </div>
-          <div class="center-content is-flex">
-            <div class="left">
-              <div class="hexagon is-flex" v-for="(feature, index) in features.left" :key="index">
-                <div>
-                  <img :src="feature.src"/>
-                  <div class="hexagon-tag">
-                    <span>{{ feature.content }}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="center">
-              <div class="hexagon is-flex" v-for="(feature, index) in features.center" :key="index">
-                <div>
-                  <img :src="feature.src"/>
-                  <div class="hexagon-tag">
-                    <span>{{ feature.content }}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="right">
-              <div class="hexagon is-flex" v-for="(feature, index) in features.right" :key="index">
-                <div>
-                  <img :src="feature.src"/>
-                  <div class="hexagon-tag">
-                    <span>{{ feature.content }}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="safe">安全</div>
-          </div>
-          
+  <div class="features">
+    <div class="item">
+      <div class="arc-middle">
+        <div class="arc-inner">
+          <img
+            src="~/assets/images/feature/async.svg"
+            alt=""
+          >
+          <div>sdaf</div>
         </div>
-    </section>
+      </div>
+      <div class="tag" style="top: 120px;left: 325px;"></div>
+      <div class="tag active" style="top: 220px;left: 325px;"></div>
+      <svg
+        class="colum-line"
+        style="top: 153px;left: 267px;"
+      >
+        <path d="M0 10 l10 -10">
+        </path>
+        <path d="M10 0 l 30 0">
+        </path>
+      </svg>
+      <svg
+        class="colum-line"
+        style="top: 232px;left: 267px;"
+      >
+        <path d="M0 0 l10 10">
+        </path>
+        <path d="M10 10 l 30 0">
+        </path>
+      </svg>
+    </div>
+        <div class="item">
+      <div class="arc-middle">
+        <div class="arc-inner">
+          <img
+            src="~/assets/images/feature/async.svg"
+            alt=""
+          >
+          <div>sdaf</div>
+        </div>
+      </div>
+      <div class="tag" style="top: 120px;left: 325px;"></div>
+      <div class="tag active" style="top: 220px;left: 325px;"></div>
+      <svg
+        class="colum-line"
+        style="top: 153px;left: 267px;"
+      >
+        <path d="M0 10 l10 -10">
+        </path>
+        <path d="M10 0 l 30 0">
+        </path>
+      </svg>
+      <svg
+        class="colum-line"
+        style="top: 232px;left: 267px;"
+      >
+        <path d="M0 0 l10 10">
+        </path>
+        <path d="M10 10 l 30 0">
+        </path>
+      </svg>
+    </div>
+        <div class="item">
+      <div class="arc-middle">
+        <div class="arc-inner">
+          <img
+            src="~/assets/images/feature/async.svg"
+            alt=""
+          >
+          <div>sdaf</div>
+        </div>
+      </div>
+      <div class="tag" style="top: 120px;left: 325px;"></div>
+      <div class="tag active" style="top: 220px;left: 325px;"></div>
+      <svg
+        class="colum-line"
+        style="top: 153px;left: 267px;"
+      >
+        <path d="M0 10 l10 -10">
+        </path>
+        <path d="M10 0 l 30 0">
+        </path>
+      </svg>
+      <svg
+        class="colum-line"
+        style="top: 232px;left: 267px;"
+      >
+        <path d="M0 0 l10 10">
+        </path>
+        <path d="M10 10 l 30 0">
+        </path>
+      </svg>
+    </div>
+        <div class="item">
+      <div class="arc-middle">
+        <div class="arc-inner">
+          <img
+            src="~/assets/images/feature/async.svg"
+            alt=""
+          >
+          <div>sdaf</div>
+        </div>
+      </div>
+      <div class="tag" style="top: 120px;left: 325px;"></div>
+      <div class="tag active" style="top: 220px;left: 325px;"></div>
+      <svg
+        class="colum-line"
+        style="top: 153px;left: 267px;"
+      >
+        <path d="M0 10 l10 -10">
+        </path>
+        <path d="M10 0 l 30 0">
+        </path>
+      </svg>
+      <svg
+        class="colum-line"
+        style="top: 232px;left: 267px;"
+      >
+        <path d="M0 0 l10 10">
+        </path>
+        <path d="M10 10 l 30 0">
+        </path>
+      </svg>
+    </div>
+        <div class="item">
+      <div class="arc-middle">
+        <div class="arc-inner">
+          <img
+            src="~/assets/images/feature/async.svg"
+            alt=""
+          >
+          <div>sdaf</div>
+        </div>
+      </div>
+      <div class="tag" style="top: 120px;left: 325px;"></div>
+      <div class="tag active" style="top: 220px;left: 325px;"></div>
+      <svg
+        class="colum-line"
+        style="top: 153px;left: 267px;"
+      >
+        <path d="M0 10 l10 -10">
+        </path>
+        <path d="M10 0 l 30 0">
+        </path>
+      </svg>
+      <svg
+        class="colum-line"
+        style="top: 232px;left: 267px;"
+      >
+        <path d="M0 0 l10 10">
+        </path>
+        <path d="M10 10 l 30 0">
+        </path>
+      </svg>
+    </div>
+    
   </div>
 </template>
 
 <script type="text/babel">
-  export default {
-    components: {
-    },
-    data: function () {
-      return {
-        features: {
-          left: [{
-            key: 'dag',
-            src: require('~/assets/images/feature/dag.svg'),
-            content: 'DAG账本'
-          }, {
-            key: 'async',
-            src: require('~/assets/images/feature/async.svg'),
-            content: '异步架构'
-          }],
-          center: [{
-            key: 'hdpos',
-            src: require('~/assets/images/feature/hdpos.svg'),
-            content: 'HDPoS 共识协议'
-          }],
-          right: [{
-            key: 'blockchain',
-            src: require('~/assets/images/feature/blockchain.svg'),
-            content: '快照链'
-          }]
-        }
+export default {
+  components: {},
+  data: function() {
+    return {
+      features: {
+        left: [
+          {
+            key: "dag",
+            src: require("~/assets/images/feature/dag.svg"),
+            content: "DAG账本"
+          },
+          {
+            key: "async",
+            src: require("~/assets/images/feature/async.svg"),
+            content: "异步架构"
+          }
+        ],
+        center: [
+          {
+            key: "hdpos",
+            src: require("~/assets/images/feature/hdpos.svg"),
+            content: "HDPoS 共识协议"
+          }
+        ],
+        right: [
+          {
+            key: "blockchain",
+            src: require("~/assets/images/feature/blockchain.svg"),
+            content: "快照链"
+          }
+        ]
       }
-    },
-    methods: {}
-  }
+    };
+  },
+  methods: {}
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  @import "~assets/vars";
-
-  section {
-      width: 100%;
-      height: 646px;
-      background-size: contain;
-      background-image: url("~assets/images/feature/bg.svg");
-      .is-flex {
-        justify-content: center;
-      }
-      .safe {
-        width: 181.5px;
-        height: 60px;
-        background-image: url("~assets/images/feature/safe.svg");
-        margin-top: 92px;
-        text-align: center;
-        line-height: 60px;
-        color: white;
-      }
-      .center-content {
+@import "~assets/vars";
+.features {
+  height: 850px;
+  padding: 0 250px 73px;
+  width: 1920px;
+  display:flex;
+flex-wrap: wrap;
+justify-content: space-between;
+  .item {
+    position: relative;
+    border-radius: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font: $font-family-title;
+    height: 400px;
+    width: 400px;
+    border: 1px dashed #007aff;
+    .arc-middle {
+      border-radius: 100%;
+      height: 286px;
+      width: 286px;
+      background: rgba(248, 251, 255, 1);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .arc-inner {
+        border-radius: 100%;
+        padding: 30px 50px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
         align-items: center;
+        color: #1b4366;
+        height: 150px;
+        width: 150px;
+        background: rgba(237, 246, 255, 1);
       }
-      .hexagon {
-        width: 226px;
-        height: 218px;
-        background: url("~assets/images/feature/hexagon.svg");
-        background-size: contain; 
-        text-align: center;
-        justify-content: center;
-        align-items: center;
-        margin-top: 40px; 
-        .hexagon-tag {
-          margin-top: 12px; 
-          font-family: $font-family-title;
-          font-size: 22px;
-          color: #171C34;
-          letter-spacing: 0.55px;
-          text-align: center;
-          width: 100%;
-          span {
-            display: inline-block;
-            width: 95px;
-            white-space: normal;
-            line-height: 30px;
-          }
-          
+    }
+    .colum-line {
+      position: absolute;
+      width: 40px;
+      height: 10px;
+      
+      path {
+        shape-rendering: crispEdges;
+        stroke: #007aff;
+        stroke-width: 4px;
+        fill: none;
+        stroke-linecap:round;
+        &:first-child{
+            stroke-width: 2px;
+            stroke-linecap:round;
         }
       }
+    }
+    .tag {
+      position: absolute;
+      width: 140px;
+      height: 60px;
+      background: rgba(255, 255, 255, 1);
+      box-shadow: 0px 0px 40px 0px rgba(211, 234, 255, 1);
+      border-radius: 30px 30px 30px 0px;
+      &.active {
+        background: linear-gradient(
+          139deg,
+          rgba(111, 191, 255, 1) 0%,
+          rgba(0, 122, 255, 1) 100%
+        );
+        color: #fff;
+      }
+    }
   }
+}
 </style>
