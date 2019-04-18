b41f926197043be1c1ff03f2aa11b40a1ca6728f
zhoudan <zhoudan@vite.org>
2019-04-18 15:05:30 +0800

    f

--- components/feature/index.vue
+++ components/feature/index.vue
@@ -265,7 +265,7 @@ export default {
     }
   }
   .feature-right {
-    margin-left: 390px;
+    margin-left: 400px;
     @media only screen and (min-width: 320px) and (max-width: 767px) {
       margin-top: 60px;
       margin-left: 155px;
